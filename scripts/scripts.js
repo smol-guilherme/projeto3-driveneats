15e19ca66ef7e8f802866755d53b438ffa749bd1
Guilherme de Oliveira <smo.guilherme93@gmail.com>
2022-04-02 13:38:30 -0300

    FrontEnd concluída. Adicionados efeitos visuais. Adicionado protótipo de requisição https para pedidos

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,6 @@
+
+
+
 function queueOrder(btnPress) {
     let orderType = btnPress.parentElement;
     let hasOrder = orderType.querySelector(".selected");
@@ -35,9 +38,51 @@ function countOrders() {
 }
 
 function paymentMenu() {
+    document.querySelector(".order-ready").disabled = true;
+    document.querySelector(".order-ready").classList.remove("order-ready");
+    document.querySelector(".checkout-menu").classList.remove("hidden");
+    document.querySelector(".content").classList.add("no-scroll");
 
+
+    const lstOrders = document.querySelectorAll(".selected");
+    const prcTable = document.querySelectorAll(".partial")
+    const lstSize = lstOrders.length;
+
+    let prcTotal = 0;
+    let itemName;
+    let priceStr;
+    let priceInt;
+
+    for (let i = 0; i < lstSize; i++) {
+        itemName = lstOrders[i].querySelector(".item-name").innerHTML;
+        priceStr = lstOrders[i].querySelector(".item-price").innerHTML;
+        prcTable[i].querySelector(".co-name").innerHTML = itemName;
+        prcTable[i].querySelector(".co-price").innerHTML = priceStr;
+        priceInt = adjustFormat(lstOrders[i].querySelector(".item-price").innerHTML)
+        prcTotal += priceInt;
+    }
+    document.querySelector(".co-total").innerHTML = prcTotal.toFixed(2).toString();
+}
+
+function adjustFormat(strToInt) {
+    return Number(strToInt.replace("," , "."))
 }
 
 function cancelOrder() {
+    document.querySelector(".content").classList.remove("no-scroll");
     document.querySelector(".checkout-menu").classList.add("hidden");
+    countOrders();
+}
+
+function requestOrder() {
+    const storeNum = "5555991449791";
+
+    const frstOrder = document.querySelector(".first-order").querySelector(".selected").querySelector(".item-name").innerHTML
+    const fstPrice = document.querySelector(".first-order").querySelector(".selected").querySelector(".item-price").innerHTML
+
+    const scndOrder = document.querySelector(".second-order").querySelector(".selected").querySelector(".item-name").innerHTML
+    const scndPrice = document.querySelector(".second-order").querySelector(".selected").querySelector(".item-price").innerHTML
+
+    const thrdOrder = document.querySelector(".third-order").querySelector(".selected").querySelector(".item-name").innerHTML
+    const thrdPrice = document.querySelector(".third-order").querySelector(".selected").querySelector(".item-price").innerHTML
 }
